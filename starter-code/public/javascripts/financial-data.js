8c4847b0b0db2ece630cac8da048887d0773a83a
Eduardo Álvarez <ceciliorubes@gmail.com>
2020-03-27 17:36:24 +0100

    Ejercicio terminado.

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,9 +1,29 @@
 let startInput = document.getElementById("start").value;
 let endInput = document.getElementById("end").value;
+let badge = document.getElementById("badges").value;
 let showButton = document.getElementById("show");
-let apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startInput}&end=${endInput}`;
+let max = document.getElementById("max");
+let min = document.getElementById("min");
+let apiUrl =
+  `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startInput}&end=${endInput}`;
 
-function graph(data) {
+function axiosCalling() {
+  axios
+    .get(apiUrl)
+    .then(responseFromAPI => {
+      let info = responseFromAPI.data.bpi;
+      let maxMin = getMaxMin(info);
+      let maxValue = maxMin[0];
+      let minValue = maxMin[1];
+      writeMaxMin(maxValue, minValue);
+      printGraph(info);
+    })
+    .catch(err => {
+      console.log("Error while getting the data: ", err);
+    });
+}
+
+function printGraph(data) {
   let keys = Object.keys(data);
   let values = Object.values(data);
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -28,25 +48,34 @@ function graph(data) {
   })
 };
 
-function axiosCalling(){
-  axios
-  .get(apiUrl)
-  .then(responseFromAPI => {
-    let info = responseFromAPI.data.bpi;
-    graph(info);
-  })
-  .catch(err => {
-    console.log("Error while getting the data: ", err);
-  });
+function getMaxMin(data) {
+  let values = Object.values(data);
+  let sortedValues = [];
+  let max = 0;
+  let min = 0;
+  let maxMin = [];
+  sortedValues = values.sort((a, b) => b - a);
+  max = sortedValues[0];
+  min = sortedValues[values.length - 1];
+  maxMin.push(max);
+  maxMin.push(min);
+  return (maxMin);
 }
 
-axiosCalling();
+function writeMaxMin(maximun, minimun) {
+  max.innerText = `${maximun}`;
+  min.innerText = `${minimun}`;
+}
 
 showButton.onclick = function (btn) {
   btn.preventDefault();
   startInput = document.getElementById("start").value;
   endInput = document.getElementById("end").value;
+  badge = document.getElementById("badges").value;
   showButton = document.getElementById("show");
-  apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startInput}&end=${endInput}`;
+  apiUrl =
+    `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startInput}&end=${endInput}&currency=${badge}`;
   axiosCalling();
 }
+
+axiosCalling();
